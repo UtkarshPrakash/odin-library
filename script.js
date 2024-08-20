936580476adb23b08dfa3a2f15d807128032d59c
UtkarshPrakash <utkarshprakash21sep@gmail.com>
2024-08-20 09:38:05 +0530

    added the basic scripts

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book() {
+    // the constructor
+}
+
+function addBookToLibrary() {
+    // adding boook
+}
+
+// function to display all the books in cards
+
+// New Book button -> brings up a form
+// Fill the author, title, num pages, read flag, etc. 
+
+// submit tries to send data to a server by default
+// need to use the event.preventDefault();
+
+// Button on book's display to remove the book from the library
+// Button on book's display to change its read status
